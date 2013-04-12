214e6bbf266ea2b2c1f42e0310b7ca852e184028
JOSHUA_LOPEZ_BINDER <YOUR_EMAIL_ADDRESS>
2013-04-12 16:38:30 -0400

    creates new meshes, subdivides edge if too long

--- meshDLA.py
+++ meshDLA.py
@@ -6,19 +6,21 @@ import math
 import time
 import Rhino
 import scriptcontext
+import System.Guid
 
 def meshDLA_MAIN():
 	#mesh = rs.GetObject("select seed mesh", filter=32, preselect=True,)
 	filter = Rhino.DocObjects.ObjectType.Mesh
-	rc, meshRef = Rhino.Input.RhinoGet.GetOneObject("select seedMesh",False,filter)
-	if not meshRef or rc!=Rhino.Commands.Result.Success: return rc
-	
-	mesh  = meshRef.Mesh()
+	rc, objRef = Rhino.Input.RhinoGet.GetOneObject("select seedMesh",False,filter)
+	if not objRef or rc!=Rhino.Commands.Result.Success: return rc
+	print "BEGIN_________________________"
+	print "type objRef: " + str(type(objRef))
+	mesh  = objRef.Mesh()
 	if not mesh: return
 
-	seedMeshID = scriptcontext.doc.Objects.Find(meshRef.ObjectId)
-	seedMesh = seedMeshID.Geometry
-	seedVerts = seedMesh.Vertices
+	# seedMeshID = scriptcontext.doc.Objects.Find(objRef.ObjectId)
+	# seedMesh = seedMeshID.Geometry
+	# seedVerts = seedMesh.Vertices
 
 	filter = Rhino.DocObjects.ObjectType.PolysrfFilter
 	rc, boundBoxRef = Rhino.Input.RhinoGet.GetOneObject("select boundingBox",False,filter)
@@ -45,8 +47,8 @@ def meshDLA_MAIN():
 	yRange = [boundBox[0].Y,boundBox[2].Y]
 	z = boundBox[4].Z
 
-	radius = .3
-	nParticles = 6
+	radius = .2
+	nParticles = 7
 	for i in range(nParticles):
 		#x = random.uniform(xMin,xMax)
 		#y = random.uniform(yMin,yMax)
@@ -56,13 +58,13 @@ def meshDLA_MAIN():
 		p.drawParticle(i)
 		particles.append(p)
 
-	nVerts = seedVerts.Count
+	nVerts = mesh.Vertices.Count
 	#print "nParticles = " + str(len(particles))
 
 	timeSteps = 100
 	for t in range(timeSteps):
-		time.sleep(0.01*10**-7)
-		Rhino.RhinoApp.Wait()
+		#time.sleep(0.01*10**-7)
+		#Rhino.RhinoApp.Wait()
 		scriptcontext.escape_test()
 		for i in range(len(particles)):
 			#print "particle#: " + str(i)
@@ -70,18 +72,81 @@ def meshDLA_MAIN():
 			p.moveParticle(speed = .08)
 			if not p.inBounds(boxID):
 				p.setToSpawnLoc(xRange,yRange,z)
-			stickIdx = p.didStick(seedVerts,nVerts)
+			stickIdx = p.didStick(mesh,nVerts)
 			if stickIdx >=0:
-				rs.AddSphere(p.posVec,p.radius)
-				p.setToSpawnLoc(xRange,yRange,z)
-				growVertice(seedMesh,stickIdx,p.posVec)
+				#rs.AddSphere(p.posVec,p.radius)
+				growVertice(objRef,mesh,stickIdx,p.posVec)
+				checkVertNeighborEdges(mesh,stickIdx,[.1,.7])
+				p.setToSpawnLoc(xRange,yRange,z)				
 			p.clearParticle()
 			p.drawParticle(i)
 
-def growVertice(seedMesh,idx,foodVec):
-	vert = seedMesh.Vertices[idx]
-	newLoc = rs.VectorAdd(vert,foodVec)
-	Rhino.Geometry.Collections.MeshVertexList.SetVertex(idx,newLoc.X,newLoc.Y,newLoc.Z)
+def growVertice(objRef, mesh,idx,foodVec):
+	#scriptcontext.doc.Objects.Delete(objRef,True)
+	vert = mesh.Vertices[idx]
+	newLoc = rs.VectorSubtract(vert,foodVec)
+	mesh.Vertices.SetVertex(idx,foodVec.X,foodVec.Y,foodVec.Z)
+	guid = scriptcontext.doc.Objects.AddMesh(mesh)
+	#print type(scriptcontext.doc.Objects)
+	#scriptcontext.doc.Views.Redraw()
+	# if scriptcontext.doc.Objects.AddMesh(mesh)!=System.Guid.Empty:
+	# 	scriptcontext.doc.Views.Redraw()
+	# 	return Rhino.Commands.Result.Success
+	# return Rhino.Commands.Result.Failure
+
+def checkVertNeighborEdges(mesh,idx,lengthRange):
+
+	minLength = lengthRange[0]
+	maxLength = lengthRange[1]
+
+	vert = mesh.Vertices[idx]
+	#rs.AddTextDot("v",vert)
+	connectedVertsIdx = mesh.Vertices.GetConnectedVertices(idx)
+	# for i in connectedVertsIdx:
+	# 	if(i!=idx):
+	# 		v = mesh.Vertices[i]
+	# 		rs.AddSphere(v,.01)
+	# print type(connectedVertsIdx)
+	# print str(connectedVertsIdx)
+	
+
+	tVertIdx = mesh.TopologyVertices.TopologyVertexIndex(idx)
+	tVert = mesh.TopologyVertices[tVertIdx]
+	assert (tVert==vert), "topolgy vert and vert not the same!"
+	vertsRepesent = mesh.TopologyVertices.MeshVertexIndices(tVertIdx)
+
+	tConnectedVerts = mesh.TopologyVertices.ConnectedTopologyVertices(tVertIdx)
+
+	#rs.AddPoint(tVert)
+	# print "meshType in neighbor check: " + str(type(mesh))
+	for neighIdx in connectedVertsIdx:
+		if(neighIdx !=idx):
+			neighVert = mesh.Vertices[neighIdx]
+			dist = rs.Distance(vert,neighVert)
+
+			distStr = "%.2f" %dist
+			#textDot = rs.AddTextDot(distStr,neighVert)
+
+			tNeighIdx = mesh.TopologyVertices.TopologyVertexIndex(neighIdx)
+			edgeIdx = mesh.TopologyEdges.GetEdgeIndex(tVertIdx,tNeighIdx)
+
+			if dist >= maxLength:
+				mesh.TopologyEdges.SplitEdge(edgeIdx,0.5)
+				guid = scriptcontext.doc.Objects.AddMesh(mesh)
+				#rs.TextObjectStyle(textDot,Italic)
+				print("long edge: %.2f" %dist)
+				#find that edge subdivide it
+				
+			elif dist <= minLength:
+				#find that edge collapse it
+
+				print("short edge: %.2f" %dist)
+				#rs.TextObjectStyle(textDot,Italic)
+
+
+
+
+
 
 	
 
@@ -118,10 +183,10 @@ class Particle:
 		y = random.uniform(yRange[0],yRange[1])
 		self.posVec =  rs.VectorCreate([x,y,z],[0,0,0])
 
-	def didStick(self,seedVerts,nVerts):
+	def didStick(self,mesh,nVerts):
 		stickIdx = -1
 		for i in range(nVerts):
-			vert = seedVerts[i]
+			vert = mesh.Vertices[i]
 			dist = rs.Distance(self.posVec,vert)
 			#take the first stick distance found
 			if(dist<=self.radius):
@@ -146,7 +211,17 @@ class Particle:
 		rs.DeleteObjects(self.geom)
 
 
-
-
 if __name__=="__main__":
 	meshDLA_MAIN()
+
+
+
+
+
+
+
+
+
+
+
+
